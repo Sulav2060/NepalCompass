09fecd62ff2715b99b8f8fb11fa00894623102d7
xdzc0 <p.friends705@gmail.com>
2024-04-19 17:52:12 +0545

    Routing for new page

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,22 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Airport from "./pages/Airport";
 import NotFound from "./pages/NotFound";
 import PageTemplate from "./pages/PageTemplate";
+import Trekking from "./pages/TrekkingHiking";
+import SafetyHealth from "./pages/SafetyHealth";
+import NatureWildlife from "./pages/NatureWildlife";
 function App() {
   return (
     <Router>
       <div>
         <Routes>
-        <Route path="" element={<Airport />} />
-        <Route path="/airport" element={<Airport />} />
-
+          <Route path="" element={<Airport />} />
+          <Route path="/arriving" element={<Airport />} />
+          <Route path="/trekking" element={<Trekking />} />
+          <Route path="/safety_health" element={<SafetyHealth />} />
+          <Route path="/nature_wildlife" element={<NatureWildlife />} />
           <Route path="/pagetemplate" element={<PageTemplate />} />
           <Route path="*" element={<NotFound />} />
-          </Routes>
+        </Routes>
       </div>
     </Router>
   );
