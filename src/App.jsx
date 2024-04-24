e8095cce1c2a744f795486be237e78d0a695a35b
sulav2060 <sulav2060@gmail.com>
2024-04-24 17:18:47 +0545

    Fix import statements in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from 'react';
+import React, { Suspense } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NotFound from "./pages/NotFound";
 import PageTemplate from "./pages/PageTemplate";
@@ -8,56 +8,56 @@ function App() {
   return (
     <Router>
       <div>
-      <Suspense>
-        {/* used suspense to stop preload of components */}
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <PageTemplate
-                contentComponent={<PageContent pagekey="Airport" />}
-                navTitle="Airport"
-              />
-            }
-          />
-          <Route
-            path="/arriving/*"
-            element={
-              <PageTemplate
-                contentComponent={<PageContent pagekey="Airport" />}
-                navTitle="Airport"
-              />
-            }
-          />
-          <Route
-            path="/trekking/*"
-            element={
-              <PageTemplate
-                contentComponent={<PageContent pagekey="Trekking" />}
-                navTitle="Trekking"
-              />
-            }
-          />
-          <Route
-            path="/safety_health/*"
-            element={
-              <PageTemplate
-                contentComponent={<PageContent pagekey="SafetyHealth" />}
-                navTitle="SafetyHealth"
-              />
-            }
-          />
-          <Route
-            path="/nature_wildlife/*"
-            element={
-              <PageTemplate
-                contentComponent={<PageContent pagekey="NatureWildlife" />}
-                navTitle="NatureWildlife"
-              />
-            }
-          />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+        <Suspense>
+          {/* used suspense to stop preload of components */}
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <PageTemplate
+                  contentComponent={<PageContent pagekey="Airport" />}
+                  navTitle="Airport"
+                />
+              }
+            />
+            <Route
+              path="/arriving/*"
+              element={
+                <PageTemplate
+                  contentComponent={<PageContent pagekey="Airport" />}
+                  navTitle="Airport"
+                />
+              }
+            />
+            <Route
+              path="/trekking/*"
+              element={
+                <PageTemplate
+                  contentComponent={<PageContent pagekey="Trekking" />}
+                  navTitle="Trekking"
+                />
+              }
+            />
+            <Route
+              path="/safety_health/*"
+              element={
+                <PageTemplate
+                  contentComponent={<PageContent pagekey="SafetyHealth" />}
+                  navTitle="SafetyHealth"
+                />
+              }
+            />
+            <Route
+              path="/nature_wildlife/*"
+              element={
+                <PageTemplate
+                  contentComponent={<PageContent pagekey="NatureWildlife" />}
+                  navTitle="NatureWildlife"
+                />
+              }
+            />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
         </Suspense>
       </div>
     </Router>
